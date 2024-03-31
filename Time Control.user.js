a4035d5b78d20dad83883e811240595368e970f4
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-31 12:57:08 -0400

    Add jshint

--- Time Control.user.js
+++ Time Control.user.js
@@ -3,7 +3,7 @@
 // @description  Script allowing you to control time.
 // @icon         https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/ce262758ff44d053136358dcd892979d_low_res_Time_Machine.png
 // @namespace    mailto:lucaszheng2011@outlook.com
-// @version      1.2.3.4b
+// @version      1.2.3.4c
 // @author       lucaszheng
 // @license      MIT
 //
@@ -16,6 +16,7 @@
 // @inject-into  page
 // @run-at       document-start
 // ==/UserScript==
+/*globals unsafeWindow,GM_setValue,GM_getValue,GM_deleteValue*/
 
 (window => {
   "use strict";
@@ -128,7 +129,7 @@
         return (baseTime == null || contTime == null) && scale == null;
       },
       set pristine(value) { if (!value) return; time.storage.reset(); },
-  
+
       get scale() {
         let scale = GM_getValue('scale', null);
         if (scale != null) return scale;
