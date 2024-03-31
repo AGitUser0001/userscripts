af69f908a561a293a549dc872054d8def42c7b9c
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-31 10:17:10 -0400

    Add @grant get,setValue

--- Time Control.user.js
+++ Time Control.user.js
@@ -3,12 +3,15 @@
 // @description  Script allowing you to control time.
 // @icon         https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/ce262758ff44d053136358dcd892979d_low_res_Time_Machine.png
 // @namespace    mailto:lucaszheng2011@outlook.com
-// @version      1.2.2.2
+// @version      1.2.2.3
 // @author       lucaszheng
 // @license      MIT
 //
 // @match        *://*/*
 // @grant        unsafeWindow
+// @grant        GM_getValue
+// @grant        GM_setValue
+
 // @inject-into  page
 // @run-at       document-start
 // ==/UserScript==
