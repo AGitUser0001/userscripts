34a007006bd57185af4e4a3a93b290a593307fed
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-31 18:48:20 -0400

    Patch and update version

--- Time Control.user.js
+++ Time Control.user.js
@@ -3,7 +3,7 @@
 // @description  Script allowing you to control time.
 // @icon         https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/ce262758ff44d053136358dcd892979d_low_res_Time_Machine.png
 // @namespace    mailto:lucaszheng2011@outlook.com
-// @version      1.2.4
+// @version      1.2.5
 // @author       lucaszheng
 // @license      MIT
 //
@@ -164,7 +164,10 @@
         let scale = GM_getValue('scale', null);
         return (baseTime == null || contTime == null) && scale == null;
       },
-      set pristine(value) { if (!value) return; time.storage.reset(); },
+      set pristine(value) {
+        if (!value) return;
+        time.storage.reset(true, true, false);
+      },
 
       get scale() {
         let scale = GM_getValue('scale', null);
