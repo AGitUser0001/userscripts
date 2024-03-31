7f22fe6cab788641c84aaecbeb085dab5f060643
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-31 13:52:29 -0400

    Refactor

--- Time Control.user.js
+++ Time Control.user.js
@@ -3,7 +3,7 @@
 // @description  Script allowing you to control time.
 // @icon         https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/ce262758ff44d053136358dcd892979d_low_res_Time_Machine.png
 // @namespace    mailto:lucaszheng2011@outlook.com
-// @version      1.2.3.5c
+// @version      1.2.3.5d
 // @author       lucaszheng
 // @license      MIT
 //
@@ -88,7 +88,7 @@
         }
         if (saveScale) {
           if (scale === 1) time.storage.reset(false, true);
-          else time.storage.scale = time.scale;
+          else time.storage.scale = scale;
         }
       },
 
