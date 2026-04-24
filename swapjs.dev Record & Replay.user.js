8a0cadcf97e3af9b10fa38ecb92549e23d2edf3f
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-04-24 18:21:55 -0400

    Remove 1px offset on hover

--- swapjs.dev Record & Replay.user.js
+++ swapjs.dev Record & Replay.user.js
@@ -4,7 +4,7 @@
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
 // @inject-into page
-// @version     1.3.1
+// @version     1.3.2
 // @author      auser0001
 // ==/UserScript==
 
@@ -1973,7 +1973,6 @@
     .rc-actions button:not([data-act="replay"]):hover:not(:disabled) {
       color: var(--dark);
       border-color: var(--accent);
-      transform: translateY(-1px);
     }
 
     .rc-actions button[data-act="replay"] {
