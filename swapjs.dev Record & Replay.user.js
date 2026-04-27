d22735579f29739d32bd47f9561be327cd5f4d9e
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-04-26 22:43:26 -0400

    Edit

--- swapjs.dev Record & Replay.user.js
+++ swapjs.dev Record & Replay.user.js
@@ -4,7 +4,7 @@
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
 // @inject-into page
-// @version     1.7.7.1
+// @version     1.7.8
 // @author      auser0001
 // ==/UserScript==
 
